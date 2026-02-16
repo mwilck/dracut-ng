fc86fc230ca3548f3cd2a60674f5b42da37888a8
Martin Wilck <mwilck@suse.com>
2026-02-16 18:40:25 +0100

    feat(nvmf): enable adapting to NBFT reconfiguration
    The cmdline() function of the nvmf module creates rd.nvmf.discover=
    cmdline entries for all discovered subsystems. But this is often
    not desirable for systems booting from an NBFT table, where the
    contents of the table may change between reboots. With hard-coded
    rd.nvmf.discover= entries, booting may fail if the NBFT configuration
    has changed.
    
    Compare the traddr/trsvcid pairs that appear in the block device
    stack with the contents of the NBFT, and don't create rd.nvmf.discover=
    entries for those that are matched.
    
    Note: This approach is simplified, as entries might differ by
    host_traddr. But such configurations are extremely rare, and
    because we don't use interface-specific routes (VRF-lite) in the
    initrd, omitting these entries can't cause boot failure, as long
    as they match by transport, traddr, and trsvcid.
    
    A follow-up patch will introduce an option to modify this behavior.
    
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- modules.d/95nvmf/module-setup.sh
+++ modules.d/95nvmf/module-setup.sh
@@ -77,6 +77,7 @@ installkernel() {
 cmdline() {
     local _hostnqn
     local _hostid
+    local -a _nbft_subsystems
 
     gen_nvmf_cmdline() {
         local _dev=$1
@@ -86,6 +87,7 @@ cmdline() {
         local trsvcid
         local _address
         local -a _address_parts
+        local nbft_entry
 
         [[ -L "/sys/dev/block/$_dev" ]] || return 0
         cd -P "/sys/dev/block/$_dev" || return 0
@@ -113,6 +115,18 @@ cmdline() {
                 [[ $i =~ ^trsvcid= ]] && trsvcid="${i#trsvcid=}"
             done
             [[ -z $traddr && -z $host_traddr && -z $trsvcid ]] && continue
+            # _nbft_subsystems is set in cmdline() scope below.
+            # It lists all subsystems found in the NBFT in the format
+            # "traddr,trsvcid"
+            # If the subsystem found is listed in the NBFT, don't add it
+            # explicitly to the command line.
+            if [[ $trtype == tcp ]]; then
+                for nbft_entry in "${_nbft_subsystems[@]}"; do
+                    if [[ "$traddr,$trsvcid" == "$nbft_entry" ]]; then
+                        continue 2
+                    fi
+                done
+            fi
             echo -n " rd.nvmf.discover=$trtype,$traddr,$host_traddr,$trsvcid"
         done
     }
@@ -127,6 +141,9 @@ cmdline() {
     fi
 
     [[ $hostonly ]] || [[ $mount_needs ]] && {
+        mapfile -t _nbft_subsystems < \
+            <(nvme nbft show -s -o json \
+                | jq -r '.[].subsystem[] | select(.transport == "tcp") | [.traddr, .trsvcid] | join(",")')
         pushd . > /dev/null
         for_each_host_dev_and_slaves gen_nvmf_cmdline
         popd > /dev/null || exit
